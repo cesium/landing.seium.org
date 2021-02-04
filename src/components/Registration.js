2f74e65fb7bacb98fb22303f6583bae5fadd9373
Jessica Lemos <jessica.andreia96@gmail.com>
2021-02-04 11:30:51 +0000

    Add Hash Code registration

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -2,13 +2,22 @@ import React from "react";
 import { Button } from "@material-ui/core";
 
 const Registration = () => (
-  <Button
-    href="http://seium21.eventbrite.pt/"
-    color="primary"
-    className="registration"
-  >
-    Inscrições
-  </Button>
+  <div className="registration-container">
+    <Button
+      href={"https://codingcompetitions.withgoogle.com/hashcode/"}
+      color="primary"
+      className="registration"
+    >
+      Hash Code
+    </Button>
+    <Button
+      href={"http://seium21.eventbrite.pt/"}
+      color="primary"
+      className="registration"
+    >
+      Inscrições
+    </Button>
+  </div>
 );
 
 export default Registration;
