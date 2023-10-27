2ec15674e8772c9490128b4e0f52c23e0f8ae417
Rui Oliveira <70754369+ruioliveira02@users.noreply.github.com>
2023-10-27 20:55:56 +0100

    chore: change to 2024 colours (#24)
    * chore: change to 2024 colours
    
    * fix: remove netlify plugin

--- layout/Home/Home.tsx
+++ layout/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Animation, SocialLinks } from "./components";
 import Head from "next/head";
+import Image from "next/image";
 
 function Home() {
   return (
@@ -7,38 +8,37 @@ function Home() {
       <Head>
         <title>Semana Engenharia Informática</title>
       </Head>
-      <div className="flex h-screen w-screen items-center bg-primary px-4">
-        <div className="relative z-50 m-auto block text-center font-iregular text-white">
+      <div className="flex h-screen w-screen items-center bg-primary px-4 font-iregular">
+        <div className="relative z-50 m-auto block text-center text-white">
           <Animation />
-          <h1 className="font-terminal-uppercase z-50 block text-3xl font-bold lg:text-5xl">
+          <h1 className="z-50 block font-terminal text-2xl font-bold uppercase lg:text-3xl">
             Semana da Engenharia Informática
           </h1>
+          <h2 className="mt-2 font-terminal text-xl text-quinary lg:text-2xl">
+            6-9 fevereiro 2024
+          </h2>
           <SocialLinks />
           <a
-            href="https://cesium.link/u/3dhgc-NsVI"
-            className="mt-2 block text-lg uppercase text-quinary underline lg:text-xl py-2"
-          >
-            Junta-te à nossa equipa
-          </a>
-          <a
             href="https://2023.seium.org/"
-            className="text-md mt-6 block uppercase lg:text-lg"
+            className="text-md lg:text-md mt-6 block uppercase"
           >
             Edição anterior
           </a>
           <a
             href="mailto:relacoes.externas@seium.org"
-            className="mt-2 block text-lg uppercase text-quinary underline lg:text-xl"
+            className="text-md mt-2 block uppercase text-quinary underline lg:text-lg"
           >
             Seja patrocinador
           </a>
         </div>
         <div className="fixed bottom-0 left-0 z-0 hidden h-auto w-1/2 p-0 lg:block">
-          <img
+          <Image
             src="/img/void.svg"
+            width={2000}
+            height={2000}
             alt="Void"
-            className="translate-y-1/5 h-auto w-full -translate-x-1/2"
-          ></img>
+            className="translate-y-1/5 m-0 h-auto w-full -translate-x-1/2"
+          />
         </div>
       </div>
     </>
