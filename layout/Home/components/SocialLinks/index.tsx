f7d8e75f6f6d6c35c2ba628dbb90a08f4c9194c8
João Lobo <30907944+joaodiaslobo@users.noreply.github.com>
2024-02-27 23:20:33 +0000

    feat: new design (#25)

--- layout/Home/components/SocialLinks/index.tsx
+++ layout/Home/components/SocialLinks/index.tsx
@@ -1,16 +1,33 @@
 import React from "react";
-import { BsFacebook, BsInstagram, BsFillEnvelopeFill } from "react-icons/bs";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsFillEnvelopeFill,
+  BsDiscord,
+} from "react-icons/bs";
 
 function SocialLinks() {
   return (
     <div className="mt-16 flex justify-center">
-      <a href="https://www.facebook.com/SEI.UMinho" className="px-4">
-        <BsFacebook size="2rem" />
-      </a>
-      <a href="https://www.instagram.com/sei.uminho" className="px-4">
+      <a
+        href="https://www.instagram.com/sei.uminho"
+        className="px-4 hover:text-quinary"
+      >
         <BsInstagram size="2rem" />
       </a>
-      <a href="mailto:geral@seium.org" className="px-4">
+      <a
+        href="https://www.facebook.com/SEI.UMinho"
+        className="px-4 hover:text-quinary"
+      >
+        <BsFacebook size="2rem" />
+      </a>
+      <a
+        href="https://discord.gg/stUtCjsnHx"
+        className="px-4 hover:text-quinary"
+      >
+        <BsDiscord size="2rem" />
+      </a>
+      <a href="mailto:geral@seium.org" className="px-4 hover:text-quinary">
         <BsFillEnvelopeFill size="2rem" />
       </a>
     </div>
